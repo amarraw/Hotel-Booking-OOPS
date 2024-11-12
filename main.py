6a838abcaa1497d8e5761a1cae649fe1d7b5dc88
amarraw <amar.raw011@gmail.com>
2024-11-12 05:55:03 +0530

    fixed bugs whith cc

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Hotel:
         self.hotel_id = hotel_id
         self.hotel_name = df.loc[df["id"] == self.hotel_id , "name" ].squeeze()
         self.city = df.loc[df["id"] == self.hotel_id , "city"].squeeze()
+        self.price = df.loc[df["id"] == self.hotel_id , "price"].squeeze()
 
     def book(self):
         # book a hotel by changing it's availability to no
@@ -26,10 +27,13 @@ class Hotel:
 
 class ReservationTicket:
 
-    def __init__(self, customer_name , hotel_obj,date ):
+    def __init__(self, customer_name , hotel_obj, date , phone_number):
         self.customer_name = customer_name
         self.hotel = hotel_obj
         self.date = date
+        self.price = hotel_obj.price
+        self.phone_number = phone_number
+        
 
     def generate(self):
         content  = f"""
@@ -38,6 +42,8 @@ class ReservationTicket:
         Name : {self.customer_name}
         Hotel Name : {self.hotel.hotel_name}
         City : {self.hotel.city}
+        Price : {self.hotel.price}
+        Phone : {self.phone_number}
         """
         return content
     
@@ -85,21 +91,46 @@ class CreditCard:
         """Check if CVC is a 3-digit or 4-digit number."""
         return cvc.isdigit() and len(cvc) in [3, 4]
 
-
+# Main program
 current_datetime = datetime.now()
 print(df)
 
-hotel_ID = input('Enter a id of the hotel : ')
+hotel_ID = input("Enter the ID of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number="4111111111111111")
-    if credit_card.validate(exp_date="12/26", holder="John Cena", cvc="123"):
+    print("---------------------Enter Passenger Details---------------------")
+    name = input("Name: ")
+    age = input("Your age: ")
+    
+    # Validate phone number
+    while True:
+        phone_number = input("Phone number (10 digits): ")
+        if phone_number.isdigit() and len(phone_number) == 10:
+            break
+        else:
+            print("Invalid phone number. Please enter a 10-digit number.")
+
+
+    print("---------------------Enter Credit Card Details---------------------")
+    while True:
+        card_number = input("Enter CC number (16 digits): ")
+        if card_number.isdigit() and len(card_number) == 16:
+            break
+        else:
+            print("Invalid card number. Please enter exactly 16 digits.")
+            
+    card_holder = input("Enter card holder name: ")
+    card_exp_date = input("Enter expiry date (MM/YY): ")
+    card_cvc = input("Enter CVC (3 or 4 digits): ")
+    credit_card = CreditCard(number=card_number)
+
+    if credit_card.validate(exp_date=card_exp_date, holder=card_holder, cvc=card_cvc):
         hotel.book()
-        name = input("Enter your name : ")
-        reservation_ticket = ReservationTicket(customer_name=name,hotel_obj=hotel,date=current_datetime)
+
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_obj=hotel, date=current_datetime, phone_number=phone_number)
         print(reservation_ticket.generate())
     else:
-        print("there is problem with payment..!")
+        print("There is a problem with the payment.")
 else:
-    print("Hotel room are full..!!")
+    print("Hotel rooms are fully booked.")
